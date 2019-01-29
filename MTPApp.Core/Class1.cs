2d5f47be6dc4f4b2378e51e0daa836c66529b8af
matebb108 <matebb108@live.com>
2019-01-29 17:43:37 +0700

    calculator

---
+++ MTPApp.Core/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MTPApp.Core
+{
+    public class Calculator
+    {
+         public int Abb (int a,int b)
+        {
+            
+            return a+b;
+        }
+    }
+}
--- MTPApp/Program.cs
+++ MTPApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MTPApp.Core;
+
 
 namespace MTPApp
 {
@@ -6,8 +8,25 @@ namespace MTPApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Input 1 = " + s1 + "");
+            string s2 = Console.ReadLine();
+            Console.WriteLine("Input 2 = " + s2 + "");
+
+
+            int a = Convert.ToInt32(s1);
+            int b = Convert.ToInt32(s2);
+
+            Calculator servive = new Calculator();
+            int c= servive.Abb(a,b);
+
+            string Messeage = $"Your Ans:{a} + {b} = {c}";
+
+            Console.WriteLine(Messeage);
             Console.ReadLine();
+
+
         }
     }
 }
